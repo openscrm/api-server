3cc4c956ba2aea87b6062d45ffdb875773545aa3
ethan <ethan@ixj.cn>
2021-08-27 15:59:35 +0800

    fix(tag): sync tag group name

--- app/models/tag_group.go
+++ app/models/tag_group.go
@@ -136,7 +136,7 @@ func (tg TagGroup) Upsert(group *TagGroup) error {
 	if len(group.Tags) > 0 {
 		err = DB.Clauses(clause.OnConflict{
 			Columns:   []clause.Column{{Name: "ext_id"}},
-			DoUpdates: clause.AssignmentColumns([]string{"create_time", "name", "order", "ext_group_id", "name"})},
+			DoUpdates: clause.AssignmentColumns([]string{"create_time", "name", "order", "ext_group_id", "group_name"})},
 		).Create(&group.Tags).Error
 		if err != nil {
 			return err
